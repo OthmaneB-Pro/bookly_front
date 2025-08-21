10f42e1c1b12a08e7069178855522262c3d0dd97
OthmaneB-Pro <othmane.baar@gmail.com>
2025-08-21 08:46:42 +0200

    ✨ F01 > Create login register and login

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [isRegister, setIsRegister] = useState(false);
+
+  const handleSubmit = () => {};
+
   return (
-    <div>LoginPage</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h1>{isRegister ? "S'inscrire" : "Se connecter"}</h1>
+
+      {isRegister && <input placeholder="Prénom" />}
+      <input placeholder="Email" />
+      <input placeholder="Mot de passe" />
+
+      <button>Valider</button>
+
+      <div>
+        {isRegister ? "Déjà un compte ?" : "Pas encore de compte ?"}
+        <span onClick={() => setIsRegister(!isRegister)}>
+          {isRegister ? "se connecter" : "s'inscrire"}
+        </span>
+      </div>
+    </form>
+  );
 }
