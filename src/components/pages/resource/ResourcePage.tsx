f7225fb67874f9ded3e58915a2c9ac41b97336ae
OthmaneB-Pro <othmane.baar@gmail.com>
2025-09-01 13:45:39 +0200

    ✨ F02 : Call Api get FindAll resource

--- src/components/pages/resource/ResourcePage.tsx
+++ src/components/pages/resource/ResourcePage.tsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
+import { findAllResource } from "../../../api/resource";
+import type { ResourceType } from "../../../types/resource";
+
 export default function ResourcePage() {
+  const [resource, setResource] = useState<ResourceType[]>();
+
+  useEffect(() => {
+    findAllResource(setResource);
+  }, []);
   return (
-    <div>ResourcePage</div>
-  )
+    <div>{resource ? resource.map((res) => <p key={res.id}>{res.title}</p>) : "non"};</div>
+  );
 }
