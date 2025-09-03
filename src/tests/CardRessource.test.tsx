c694faea9536d2f6a17dce0f57a6f0282c481456
OthmaneB-Pro <othmane.baar@gmail.com>
2025-09-03 19:30:09 +0200

    🧪 F02 : test > CardResource

---
+++ src/tests/CardRessource.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { describe, test, vi } from "vitest";
+import CardResource from "../components/pages/resource/card/CardResource";
+import { mockResources } from "./mockResource";
+
+vi.mock("../api/resource", () => ({
+  findAllResource: jest.fn((setResource) => {
+    setResource(mockResources);
+  }),
+}));
+
+describe("New test for CardResource", () => {
+  test("EmptyResource render the good text ", () => {
+    render(<CardResource />);
+    const EmptyTextElement = screen.getByText("Aucune ressource actuellement");
+    expect(EmptyTextElement).toBeInTheDocument();
+  });
+  test.only("affiche les ressources après l'appel API ", async () => {
+    render(<CardResource />);
+
+    await waitFor(() => {
+      expect(screen.getByText("Salle de conférence")).toBeInTheDocument();
+      expect(screen.getByText("Event")).toBeInTheDocument();
+    });
+  });
+});
