f8f7f95d3fc743dc5b59422a2f23b6bf98771ab6
OthmaneB-Pro <othmane.baar@gmail.com>
2025-09-07 18:00:04 +0200

    ✨ F03 : request create new resource

--- src/api/resource.ts
+++ src/api/resource.ts
@@ -3,16 +3,30 @@ import type { ResourceType } from "../types/resource";
 
 const token = localStorage.getItem("jwt");
 
-export const findAllResource = async (setResource : React.Dispatch<React.SetStateAction<ResourceType[] | undefined>>) => {
+export const findAllResource = async (
+  setResource: React.Dispatch<React.SetStateAction<ResourceType[] | undefined>>
+) => {
   try {
     const res = await axios.get("http://localhost:8080/resource", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-      
     });
 
-    setResource(res.data)
+    setResource(res.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const createResource = async () => {
+  try {
+    const res = await axios.post("http://localhost:8080/resource", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
   } catch (err) {
     console.error(err);
   }
