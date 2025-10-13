dfb32f5bd958aab9ab7e471ea189cd7de1e63b1c
OthmaneB-Pro <othmane.baar@gmail.com>
2025-10-13 21:28:45 +0200

    🧪 F03 : Test fall

--- src/tests/FormResource.test.tsx
+++ src/tests/FormResource.test.tsx
@@ -1,7 +1,20 @@
-import { render } from "@testing-library/react"
-import FormResource from "../components/pages/resource/resourceForm/FormResource"
+import { render, screen } from "@testing-library/react";
+import FormResource from "../components/pages/resource/resourceForm/FormResource";
+import { ResourceContext } from "../context/ResourceContext";
+import userEvent from "@testing-library/user-event";
 
-test("Test formulaire -> création d'une ressource ", () => {
-    render(<FormResource />)
-     
-})
+test("Test formulaire -> création d'une ressource ", async () => {
+  const mockSetResource = jest.fn();
+
+  render(
+    <ResourceContext.Provider
+      value={{ setResource: mockSetResource, resource: [] }}
+    >
+      <FormResource />
+    </ResourceContext.Provider>
+  );
+  const user = userEvent.setup();
+  await user.click(screen.getByRole("button"));
+  const ButtonForm = screen.getByLabelText(/Valider/i);
+  expect(ButtonForm).toBeInTheDocument(); 
+});
